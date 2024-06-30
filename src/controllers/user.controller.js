1fd9c6057bff43fd5edb20a77b16822a29e8d843
ravinsan <39220869+ravinsan@users.noreply.github.com>
2024-07-01 00:01:55 +0530

    add user password hashing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,19 +1,23 @@
 import User from "../models/user.model.js";
+import becrypt from "bcrypt";
 
 export const signUp = async (req, res)=>{
     const {name, email, password, mobile, status} = req.body;
+    const hashedPassword = await becrypt.hash(password, 10);
+    
+
     try{
         const user = await User.create({
             name,
             email,
-            password,
+            password:hashedPassword,
             mobile,
             status
         });
 
         res.status(201).json({
             message: "User created successfully",
-            user
+            // user
         });
     }
     catch(error){
